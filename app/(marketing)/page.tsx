b06bb5fe658fb42a0225f8f5aff6978615c540ae
bharadwaj1118 <bharadwaj.kanukoti@gmail.com>
2023-11-26 17:28:08 +0100

    changed hero section

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 
 import { siteConfig } from '@/config/site';
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
 import { buttonVariants } from '@/components/ui/button';
 import { CaretSortIcon } from '@radix-ui/react-icons';
 
@@ -45,27 +46,46 @@ export default async function IndexPage() {
 
   return (
     <>
-      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32 h-screen">
-        <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
-          <Link
-            href={siteConfig.links.twitter}
-            className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
-            target="_blank"
-          >
-            Follow along on Twitter
-          </Link>
-          <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
-            The CX community that grows your career.
+      <section className="container h-screen grid lg:grid-cols-2 place-items-center pt-16 pb-8 md:pt-8">
+        <div className="py-6 md:order-1 hidden md:block">
+          <Image
+            src="/hero.png"
+            alt="Astronaut in the air"
+            loading="eager"
+            priority={true}
+            width={500}
+            height={500}
+          />
+        </div>
+        <div>
+          <h1 className="text-5xl lg:text-6xl xl:text-7xl font-bold lg:tracking-tight">
+            The CX Community that grows your career
           </h1>
-          <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+          <p className="text-lg mt-4 text-slate-600 max-w-xl">
             Your gateway to a community that thrives on learning and growth.
+            <wbr /> Built with Next.js, TailwindCSS & Alpine.js. You can quickly
+            grow your customer experience career with this website.
           </p>
-          <div className="space-x-4">
+          <div className="mt-6 flex flex-col sm:flex-row gap-3">
             <Link
               href="/profile"
               className={cn(buttonVariants({ size: 'lg' }))}
             >
               Grow your career
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 pl-2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
+                />
+              </svg>
             </Link>
             <Link
               href={siteConfig.links.github}
