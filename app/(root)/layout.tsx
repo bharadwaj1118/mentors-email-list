65b8a1d197dc6d99e3ea5b554332a6f542149567
bharadwaj1118 <bharadwaj.kanukoti@gmail.com>
2023-11-28 11:26:02 +0100

    added not found

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
         <aside className="fixed top-14 z-30 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 overflow-y-auto border-r py-6 pr-2 md:sticky md:block lg:py-10">
           <DocsSidebarNav items={docsConfig.sidebarNav} />
         </aside>
-        <div className="w-5/6 pt-6">
+        <div className="w-5/6">
           <main>{children}</main>
         </div>
       </div>
