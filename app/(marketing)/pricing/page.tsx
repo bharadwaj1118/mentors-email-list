245bd338611af0e966bd1584f865e4e824af1519
bharadwaj1118 <bharadwaj.kanukoti@gmail.com>
2023-11-28 11:02:25 +0100

    Added purchase

--- app/(marketing)/pricing/page.tsx
+++ app/(marketing)/pricing/page.tsx
@@ -1,93 +1,14 @@
-import Link from 'next/link';
-
-import { cn } from '@/lib/utils';
-import { buttonVariants } from '@/components/ui/button';
-import { Icons } from '@/components/icons';
-import Pricing from '@/components/pricing';
-
 export const metadata = {
-  title: 'Pricing',
+  title: 'Pricing - Simple',
+  description: 'Page description',
 };
 
-const pricing = [
-  {
-    name: 'Eclipse',
-    price: {
-      monthly: '$39',
-      annual: '$16',
-      discount: '10%',
-      original: '$24',
-    },
-    popular: false,
-    features: [
-      '4 calls/month',
-      'Access to all mentors',
-      'Chat with mentors before booking',
-      'Slack community',
-      'Customer support',
-    ],
-    button: {
-      text: 'Get Started',
-      link: '/',
-    },
-  },
-  {
-    name: 'Moon',
-    price: {
-      monthly: '$59',
-      annual: '$16',
-      discount: '10%',
-      original: '$24',
-    },
-    popular: true,
-    features: [
-      'Everything in Eclipse',
-      'Unlimited calls/month',
-      'Personalized onboarding',
-      'Unlimited Collaborators',
-      'Priority Support',
-    ],
-    button: {
-      text: 'Get Started',
-      link: '#',
-    },
-  },
-  {
-    name: 'Sun',
-    price: {
-      monthly: '$79',
-      annual: '$16',
-      discount: '10%',
-      original: '$24',
-    },
-    popular: false,
-    features: [
-      'Everything in Moon',
-      'Access to deals on tools',
-      'Dedicated Account Manager',
-      '24/7 Phone Support',
-    ],
-    button: {
-      text: 'Get started',
-      link: '/contact',
-    },
-  },
-];
+import PricingTables from './pricing-table';
 
-export default function PricingPage() {
+export default function Pricing() {
   return (
-    <section className="container">
-      <div className="flex items-center justify-center flex-col ">
-        <h2 className="font-bold text-3xl text-gray-800">Pricing</h2>
-        <p className="text-lg leading-relaxed text-slate-500 mt-3">
-          Simple & Predictable pricing. No Surprises.
-        </p>
-      </div>
-      <div className="grid md:grid-cols-3 gap-10 mx-auto max-w-screen-lg mt-12">
-        {pricing.map((item, index) => (
-          <Pricing plan={item} key={index} />
-        ))}
-      </div>
-    </section>
+    <>
+      <PricingTables />
+    </>
   );
 }
