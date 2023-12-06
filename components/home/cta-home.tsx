d3047912623a498a5c8c47c0bcfc8a4dd04dd2cb
bharadwaj1118 <bharadwaj.kanukoti@gmail.com>
2023-12-06 11:18:51 +0100

    Modified CTA content

--- components/home/cta-home.tsx
+++ components/home/cta-home.tsx
@@ -5,14 +5,18 @@ export default function CtaDark() {
     <section className="bg-slate-900">
       <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
         <div className="py-12 md:py-20">
-          <div className="relative max-w-3xl mx-auto text-center">
+          <div className="relative max-w-5xl mx-auto text-center">
             <div className="relative">
               <h2 className="h2 font-playfair-display text-slate-100 mb-4">
-                Say goodbye to slow learnings, big updates, and{' '}
-                <span className="text-emerald-500">confusion</span>.
+                Say goodbye to roadblocks, guesswork, and frustration.
               </h2>
+
               <p className="text-xl text-slate-400 mb-8">
-                Join Mentors CX and start building your career today.
+                Join Mentors CX and start{' '}
+                <span className="text-emerald-500">
+                  building your career today
+                </span>
+                .
               </p>
               <div>
                 <Link
