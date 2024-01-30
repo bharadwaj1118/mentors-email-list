aa3d1a2c024b3a3f62cf4c0425cb2ccc48e68127
bharadwaj1118 <bharadwaj.kanukoti@gmail.com>
2024-01-30 09:27:57 +0530

    added blogging

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+const { withContentlayer } = require("next-contentlayer");
+
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['img.clerk.com', /*...other domains if any*/],
+    domains: ["img.clerk.com" /*...other domains if any*/],
   },
 };
 
-module.exports = nextConfig;
+module.exports = withContentlayer(nextConfig);
