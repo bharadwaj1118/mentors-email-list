c79d4fbbb483fa8f786dc60cd54047aabf8e525d
bharadwaj1118 <bharadwaj.kanukoti@gmail.com>
2024-05-26 20:35:58 -0500

    added the prisma

--- lib/actions/admin.action.ts
+++ lib/actions/admin.action.ts
@@ -37,6 +37,29 @@ export async function updateMentorApplicationStatus({
       },
     });
 
+    if (applicationStatus === "ACCEPTED") {
+      const user = await db.user.findUnique({
+        where: {
+          email: application.email,
+        },
+      });
+
+      if (user) {
+        await db.user.update({
+          where: {
+            id: user.id,
+          },
+          data: {
+            username: `${application.firstname} ${application.lastname}`,
+            bio: application?.profileStatement,
+            linkedinProfile: application?.linkedinUrl,
+            maxSessions: Number(application?.weeklySessions) || 5,
+            role: "MENTOR",
+          },
+        });
+      }
+    }
+
     return application;
   } catch (error) {
     console.error(error);
